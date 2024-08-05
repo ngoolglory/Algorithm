18c0c72eef8fac1679311ae64ec268a6ae572ea8
ngoolglory <ngoolseo@gmail.com>
2024-08-05 17:50:53 +0900

    수정

--- SSAFY/S_string/palindrome2.py
+++ SSAFY/S_string/palindrome2.py
@@ -9,12 +9,16 @@ def len_of_longest_palindrome(board):
     # 가로
     for r in range(100):
         for c in range(100):
-            for end in range(100, c, -1):       # 회문인지 탐색할 문자열 대상의 끝 지점
-                string = board[r][c:end]        # 현지점부터 끝지점까지의 문자열을 가져오기
-                if string == string[::-1]:      # 가져온 문자열이 회문이면
-                    if len(string) > max_len:   # 길이가 기존 최대 길이보다 크면
-                        max_len = len(string)   # 최대 길이 갱신
+            for end in range(100, c, -1):   # 회문인지 탐색할 문자열 대상의 끝 지점
+                if (end - c) <= max_len:    # 현재 길이가 최대 길이보다 작거나 같으면 종료
                     break
+                for i in range((end-c)//2):
+                    if board[r][c + i] != board[r][end-i-1]:  # 회문 체크
+                        break
+                else:
+                    curr_len = end - c
+                    if max_len < curr_len:
+                        max_len = curr_len
 
     # 전치행렬 구하기
     for r in range(100):
@@ -26,11 +30,15 @@ def len_of_longest_palindrome(board):
     for r in range(100):
         for c in range(100):
             for end in range(100, c, -1):  # 회문인지 탐색할 문자열 대상의 끝 지점
-                string = board[r][c:end]  # 현지점부터 끝지점까지의 문자열을 가져오기
-                if string == string[::-1]:  # 가져온 문자열이 회문이면
-                    if len(string) > max_len:  # 길이가 기존 최대 길이보다 크면
-                        max_len = len(string)  # 최대 길이 갱신
+                if (end - c) <= max_len:  # 현재 길이가 최대 길이보다 작거나 같으면 종료
                     break
+                for i in range((end - c) // 2):
+                    if board[r][c + i] != board[r][end - i - 1]:  # 회문 체크
+                        break
+                else:
+                    curr_len = end - c
+                    if max_len < curr_len:
+                        max_len = curr_len
 
     return max_len
 
