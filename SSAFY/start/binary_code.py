0d21436ba4e086f118c2b94073b5272657bc58f0
ngoolglory <joungnx123@naver.com>
2024-08-29 17:28:19 +0900

    이진 암호코드

--- SSAFY/start/binary_code.py
+++ SSAFY/start/binary_code.py
@@ -20,34 +20,27 @@ def code_check(code):
     return 0                        # 10의 배수 아니면 잘못된 암호코드이므로 0 반환
 
 
-
 decoder = {'0001101' : 0, '0011001' : 1, '0010011' : 2, '0111101' : 3, '0100011' : 4,
            '0110001' : 5,'0101111' : 6, '0111011' : 7, '0110111' : 8, '0001011' : 9}
 
 for tc in range(1, 1+int(input())):
-    H, W = map(int, input().split())                            # H: 배열 세로, W: 배열 가로
+    H, W = map(int, input().split())            # H: 배열 세로, W: 배열 가로
     arr = [list(input()) for _ in range(H)]     # 배열
 
     # 암호가 있는 한 행만 보면 되니까 그 행이 어딘지 찾기
     find_flag = False
     for r in range(H):
-        for c in range(W):
-            if arr[r][c] == '1':
+        for c in range(W-1, -1, -1):    # 암호의 마지막은 1이기 때문에 뒤에서부터 읽기
+            if arr[r][c] == '1':        # 해당 행에서 가장 마지막에 있는 1이 암호의 끝
                 find_flag = True
                 break
         if find_flag:
             break
     
-    code_row = arr[r]                                   # 암호가 있는 행
-    for i in range(0, W - 56 + 1):                      # 시작 지점이 될 수 있는 인덱스들 순회
-        code = []                                       # 완성된 코드를 넣을 리스트 초기화
-        for start in range(i, i + 56, 7):               # 시작 지점으로부터 56칸까지 7칸씩 점프
-            key = ''.join(code_row[start:start+7])      # 7개의 비트
-            if key not in decoder:                      # 해당 키가 암호 종류 내에 없으면
-                break                                   # 다음 시작 지점을 탐색
-            else:                                       # 해당 키가 암호 종류 내에 있으면
-                code.append(decoder[key])               # 키를 해독 후 코드에 넣기
-        if len(code) == 8:                              # 8자리 코드가 완성됐으면
-            break                                       # 탐색 종료
+    code_row = arr[r][c+1-56:c+1]                   # 암호 비트 부분만 가져오기
+    code = []                                       # 완성된 코드를 넣을 리스트 초기화
+    for start in range(0, len(code_row), 7):        # 7칸씩 점프하며 시작지점 설정
+        key = ''.join(code_row[start:start+7])      # 7개의 비트
+        code.append(decoder[key])                   # 키를 해독 후 코드에 넣기
 
     print(f'#{tc}', code_check(code))
