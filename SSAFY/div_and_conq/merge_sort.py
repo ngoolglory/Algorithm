7788be8c2bacbc0e4a7e97ff4692a072fdebde52
ngoolglory <ngoolseo@gmail.com>
2024-09-04 15:25:21 +0900

    병합정렬

---
+++ SSAFY/div_and_conq/merge_sort.py
@@ -0,0 +1,62 @@
+# # 분할정복_병합정렬
+import sys
+sys.stdin = open('C:/Users/SSAFY/Downloads/sample_input.txt', 'r')
+#sys.stdin = open("C:/Users/82108/Downloads/sample_input.txt", "r")
+
+def merge_sort(m):
+    global cnt
+    # 리스트의 길이가 1이면 이미 정렬된 상태이므로 그대로 반환
+    if len(m) == 1:
+        return m
+
+    # 리스트를 절반으로 나누기 위해 중간 인덱스를 계산
+    mid = len(m) // 2
+    left = m[:mid]  # 리스트의 앞쪽 절반
+    right = m[mid:]  # 리스트의 뒤쪽 절반
+
+    # 재귀적으로 왼쪽 부분과 오른쪽 부분을 정렬
+    left = merge_sort(left)
+    right = merge_sort(right)
+
+    if left[-1] > right[-1]:
+        cnt += 1                # 카운트 1 올려
+
+    # 두 개의 정렬된 리스트를 병합하여 반환
+    return merge(left, right)
+
+
+def merge(left, right):
+    # 두 리스트를 병합할 결과 리스트를 초기화
+    result = [0] * (len(left) + len(right))
+    l = r = 0  # 왼쪽 리스트와 오른쪽 리스트의 인덱스
+
+    # 두 리스트를 순차적으로 비교하여 작은 값을 결과 리스트에 추가
+    while l < len(left) and r < len(right):
+        if left[l] < right[r]:
+            result[l + r] = left[l]
+            l += 1
+        else:
+            result[l + r] = right[r]
+            r += 1
+
+    # 왼쪽 리스트에 남은 요소들을 결과 리스트에 추가
+    while l < len(left):
+        result[l + r] = left[l]
+        l += 1
+
+    # 오른쪽 리스트에 남은 요소들을 결과 리스트에 추가
+    while r < len(right):
+        result[l + r] = right[r]
+        r += 1
+
+    # 병합된 결과 리스트를 반환
+    return result
+
+
+for tc in range(1, int(input())+1):
+    N = int(input())
+    arr = list(map(int, input().split()))
+    cnt = 0                         # 왼쪽 마지막 원소가 오른쪽 마지막 원소보다 큰 경우 카운트 변수
+
+    sorted_lst = merge_sort(arr)
+    print(f'#{tc}', sorted_lst[N//2], cnt)
