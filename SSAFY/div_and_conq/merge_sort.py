70f8ad57267727e0b545be1d34648308fbcff247
ngoolglory <ngoolseo@gmail.com>
2024-09-05 13:34:29 +0900

    병합정렬

--- SSAFY/div_and_conq/merge_sort.py
+++ SSAFY/div_and_conq/merge_sort.py
@@ -4,7 +4,6 @@ sys.stdin = open('C:/Users/SSAFY/Downloads/sample_input.txt', 'r')
 #sys.stdin = open("C:/Users/82108/Downloads/sample_input.txt", "r")
 
 def merge_sort(m):
-    global cnt
     # 리스트의 길이가 1이면 이미 정렬된 상태이므로 그대로 반환
     if len(m) == 1:
         return m
@@ -18,14 +17,12 @@ def merge_sort(m):
     left = merge_sort(left)
     right = merge_sort(right)
 
-    if left[-1] > right[-1]:
-        cnt += 1                # 카운트 1 올려
-
     # 두 개의 정렬된 리스트를 병합하여 반환
     return merge(left, right)
 
 
 def merge(left, right):
+    global cnt
     # 두 리스트를 병합할 결과 리스트를 초기화
     result = [0] * (len(left) + len(right))
     l = r = 0  # 왼쪽 리스트와 오른쪽 리스트의 인덱스
@@ -48,6 +45,9 @@ def merge(left, right):
     while r < len(right):
         result[l + r] = right[r]
         r += 1
+
+    if left[-1] > right[-1]:
+        cnt += 1                # 카운트 1 올려
 
     # 병합된 결과 리스트를 반환
     return result
